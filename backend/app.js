d9566a51120d881f7f072092d715abfcb0a42ff9
Ming Zhang <mzhang630@gmail.com>
2022-12-19 09:19:30 -0500

    mern twitter pushed to github

---
+++ backend/app.js
@@ -0,0 +1,100 @@
+const express = require("express");
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const debug = require('debug');
+
+const cors = require('cors');
+const csurf = require('csurf');
+const { isProduction } = require('./config/keys');
+
+require('./models/User');
+require('./models/Tweet');
+require('./config/passport'); // <-- ADD THIS LINE
+const passport = require('passport'); // <-- ADD THIS LINE
+
+
+const usersRouter = require('./routes/api/users'); // update the import file path
+const tweetsRouter = require('./routes/api/tweets'); // update the import file path
+const csrfRouter = require('./routes/api/csrf');
+
+const app = express();
+
+app.use(logger('dev')); // log request components (URL/method) to terminal
+app.use(express.json()); // parse JSON request body
+app.use(express.urlencoded({ extended: false })); // parse urlencoded request body
+app.use(cookieParser()); // parse cookies as an object on req.cookies
+
+// Set the _csrf token and create req.csrfToken method to generate a hashed
+// CSRF token
+app.use(
+    csurf({
+      cookie: {
+        secure: isProduction,
+        sameSite: isProduction && "Lax",
+        httpOnly: true
+      }
+    })
+);
+
+// Attach Express routers
+app.use('/api/users', usersRouter); // update the path
+app.use('/api/tweets', tweetsRouter); // update the path
+app.use('/api/csrf', csrfRouter);
+
+// Serve static React build files statically in production
+if (isProduction) {
+    const path = require('path');
+    // Serve the frontend's index.html file at the root route
+    app.get('/', (req, res) => {
+      res.cookie('CSRF-TOKEN', req.csrfToken());
+      res.sendFile(
+        path.resolve(__dirname, '../frontend', 'build', 'index.html')
+      );
+    });
+  
+    // Serve the static assets in the frontend's build folder
+    app.use(express.static(path.resolve("../frontend/build")));
+  
+    // Serve the frontend's index.html file at all other routes NOT starting with /api
+    app.get(/^(?!\/?api).*/, (req, res) => {
+      res.cookie('CSRF-TOKEN', req.csrfToken());
+      res.sendFile(
+        path.resolve(__dirname, '../frontend', 'build', 'index.html')
+      );
+    });
+  }
+
+// add this line right before the CORS and CSRF configurations
+app.use(passport.initialize());
+
+// ADD THIS SECURITY MIDDLEWARE
+// Security Middleware
+if (!isProduction) {
+    // Enable CORS only in development because React will be on the React
+    // development server (http://localhost:3000). (In production, the Express 
+    // server will serve the React files statically.)
+    app.use(cors());
+}
+
+// Express custom middleware for catching all unmatched requests and formatting
+// a 404 error to be sent as the response.
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.statusCode = 404;
+    next(err);
+});
+
+const serverErrorLogger = debug('backend:error');
+
+app.use((err, req, res, next) => {
+    serverErrorLogger(err);
+    const statusCode = err.statusCode || 500;
+    res.status(statusCode);
+    res.json({
+      message: err.message,
+      statusCode,
+      errors: err.errors
+    })
+});
+
+module.exports = app;
